ba20b2f50e1884879e5d5355ebff4dab7c8b92ca
yoshiyasu-chonan <87126560+yoshiyasu-chonan@users.noreply.github.com>
2021-07-08 20:14:26 +0900

    User name added

--- ex4.c
+++ ex4.c
@@ -4,6 +4,13 @@
 int main(){
   int i, num, heads=0, tails=0;
   
+  //名前を聞くパート
+  char str[8];
+  printf("あんた誰? ");
+  scanf("%s", str);
+  printf("ハロー、%s\n", str);
+    
+  //コイントスのパート  
   printf("Tossing a coin...\n");
   
   for(i = 0; i < 3; i++){
